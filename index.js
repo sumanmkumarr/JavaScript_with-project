eef21e0a3acb2d97f8c185bbf2387e54b2852c03
suman kumar <sumang91222@gmail.com>
2024-08-10 17:58:47 +0530

    code

---
+++ index.js
@@ -0,0 +1,40 @@
+// 1).ways to print javascript 
+//console.log("hello world");
+// alert("me");
+//document.write("this is document write")// it is for write on document
+
+// 2).javascript console api(application programing interface)
+// console.log("hello world", 8 + 4, "this is another log");
+// console.warn('this is warning');
+// console.error('this is an error');
+
+
+//3).javascript variable
+// variable- container which store the data value
+// var is datatype 
+// var number1=9;
+// var number2=8;
+// console.log(number1+number2);
+
+
+// 4) data type in javascript
+//nums
+// var num1=7;
+
+
+// //string
+// var str1="werwt";
+
+
+// //object-it is key value pair in javascript
+// var marks={
+//     suman:99,
+//     aman:89,
+//     siki:99
+// };
+// console.log(marks);
+
+//booleans
+var a=true;
+var b=false;
+console.log(a,b);
